2e3dda6105edf82401f0f0a07d8858b51f202b37
cheesydoritos08 <163181834+cheesydoritos08@users.noreply.github.com>
2025-06-15 23:57:29 -0400

    Made sure not to round claim_time_timestamp twice

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def on_dbl_vote(data):
             await member.send(f"You have received 2000 yen and a {character['name']} shard from voting!")
             database_handler.users.update_one({"_id": user_id}, {"$set": {"vote.last_vote_time": claim_time}})
             
-            Timer(user_id=user_id, name="bot_vote", starttime=round(claim_time_timestamp), timer_length=60 * 60 * 12)
+            Timer(user_id=user_id, name="bot_vote", starttime=claim_time_timestamp, timer_length=60 * 60 * 12)
             
         except Exception as e:
             create_error_embed(error=e)
